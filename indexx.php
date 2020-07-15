da1dd00b8f39bc6555bf6e81ccc171d3eeeb7b16
Sahiba31 <64470245+Sahiba31@users.noreply.github.com>
2020-07-15 16:28:38 +0530

    hi

--- indexx.php
+++ indexx.php
@@ -4,7 +4,7 @@ Welcome
 <body>
 <?php
   print `date`;
-echo "<img src = 'https://sahiba1-bucket1.s3.ap-south-1.amazonaws.com/Friends.jpg' , width='450' , height='500' />";
+echo "<img src = 'https://d2x7z5jkbwyi1r.cloudfront.net/Friends.jpg' , width='450' , height='500' />";
 ?>
 </body>
 </html>
